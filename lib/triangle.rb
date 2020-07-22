08e846db7b1c5e26331a8813fd605a3f02b40132
Jackson Reynolds <jwreynold@gmail.com>
2020-07-22 02:49:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
   def kind
     if self.side1 < 0 || self.side2 < 0 || self.side3 < 0 || self.side1 + self.side2 < self.side3 ||
        self.side2 + self.side3 < self.side1 || self.side1 + self.side3 < side2
-       puts "i hit!"
+      #  puts "i hit!"
        begin
          raise TriangleError
        rescue TriangleError => error
